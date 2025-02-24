b8388ddb697bf344b2cd7b71f6ad6ee2faf7afe5
Ahmed Shaheen <40238286+Ashaheen892@users.noreply.github.com>
2025-02-24 16:54:15 +0200

    cleaning_instructions_category

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -14,6 +14,7 @@ class Product extends BasePage {
     this.initProductOptionValidations();
     this.displayProductOptions();
     this.tabAccordion();
+    this.initFeaturedTabs();
 
     if (imageZoom) {
       // call the function when the page is ready
@@ -293,6 +294,20 @@ class Product extends BasePage {
         icon.classList.toggle('sicon-add', !open);
       }
     }
+  }
+  initFeaturedTabs() {
+      app.all('.tab-trigger', el => {
+          el.addEventListener('click', ({ currentTarget: btn }) => {
+              let id = btn.dataset.componentId;
+              // btn.setAttribute('fill', 'solid');
+              app.toggleClassIf(`#${id} .tabs-wrapper>div`, 'is-active opacity-0 translate-y-3', 'inactive', tab => tab.id == btn.dataset.target)
+                  .toggleClassIf(`#${id} .tab-trigger`, 'is-active', 'inactive', tabBtn => tabBtn == btn);
+
+              // fadeIn active tabe
+              setTimeout(() => app.toggleClassIf(`#${id} .tabs-wrapper>div`, 'opacity-100 translate-y-0', 'opacity-0 translate-y-3', tab => tab.id == btn.dataset.target), 100);
+          })
+      });
+      document.querySelectorAll('.s-block-tabs').forEach(block => block.classList.add('tabs-initialized'));
   }
 }
 
