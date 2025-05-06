77d9da8cc1290011377016b5de81e06a91b007e2
Ahmed Shaheen <40238286+Ashaheen892@users.noreply.github.com>
2025-05-06 09:04:02 +0300

    home

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -14,56 +14,84 @@ class Home extends BasePage {
    * used in views/components/home/featured-products-style*.twig
    */
   initFeaturedTabs() {
-    app.all('.tab-trigger', el => {
+    app.all('.tab-trigger', (el) => {
       el.addEventListener('click', ({ currentTarget: btn }) => {
         let id = btn.dataset.componentId;
         // btn.setAttribute('fill', 'solid');
-        app.toggleClassIf(`#${id} .tabs-wrapper>div`, 'is-active opacity-0 translate-y-3', 'inactive', tab => tab.id == btn.dataset.target)
-          .toggleClassIf(`#${id} .tab-trigger`, 'is-active', 'inactive', tabBtn => tabBtn == btn);
+        app
+          .toggleClassIf(
+            `#${id} .tabs-wrapper>div`,
+            'is-active opacity-0 translate-y-3',
+            'inactive',
+            (tab) => tab.id == btn.dataset.target
+          )
+          .toggleClassIf(
+            `#${id} .tab-trigger`,
+            'is-active',
+            'inactive',
+            (tabBtn) => tabBtn == btn
+          );
 
         // fadeIn active tabe
-        setTimeout(() => app.toggleClassIf(`#${id} .tabs-wrapper>div`, 'opacity-100 translate-y-0', 'opacity-0 translate-y-3', tab => tab.id == btn.dataset.target), 100);
-      })
+        setTimeout(
+          () =>
+            app.toggleClassIf(
+              `#${id} .tabs-wrapper>div`,
+              'opacity-100 translate-y-0',
+              'opacity-0 translate-y-3',
+              (tab) => tab.id == btn.dataset.target
+            ),
+          100
+        );
+      });
     });
-    document.querySelectorAll('.s-block-tabs').forEach(block => block.classList.add('tabs-initialized'));
+    document
+      .querySelectorAll('.s-block-tabs')
+      .forEach((block) => block.classList.add('tabs-initialized'));
   }
 
   circleBullets() {
     function addSVGToBullets() {
-      document.querySelectorAll(".home-main-slider .swiper-pagination-bullet").forEach(function (bullet) {
-        if (!bullet.querySelector("svg")) {
-          bullet.innerHTML = `
+      document
+        .querySelectorAll('.home-main-slider .swiper-pagination-bullet')
+        .forEach(function (bullet) {
+          if (!bullet.querySelector('svg')) {
+            bullet.innerHTML = `
                 <svg width="24" height="24" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="18" cy="18" r="16" fill="none" stroke="#fff" stroke-width="2"></circle>
                     <circle class="progress-ring" cx="18" cy="18" r="16" fill="none" stroke="#fff" stroke-width="4" stroke-dasharray="100" stroke-dashoffset="100"></circle>
                 </svg>
             `;
-        }
-      });
+          }
+        });
     }
     const observer = new MutationObserver(addSVGToBullets);
     observer.observe(document.body, { childList: true, subtree: true });
-    document.addEventListener("DOMContentLoaded", addSVGToBullets);
+    document.addEventListener('DOMContentLoaded', addSVGToBullets);
     // restartAutoplayOnClick
     function restartAutoplayOnClick(swiperInstance, sliderElement) {
-      sliderElement.querySelectorAll('.swiper-pagination-bullet ,.s-slider-nav-arrow').forEach((bullet) => {
-        bullet.addEventListener('click', function () {
-          setTimeout(() => {
-            if (swiperInstance && swiperInstance.autoplay) {
-              swiperInstance.autoplay.start();
-            }
-          }, 500);
+      sliderElement
+        .querySelectorAll('.swiper-pagination-bullet ,.s-slider-nav-arrow')
+        .forEach((bullet) => {
+          bullet.addEventListener('click', function () {
+            setTimeout(() => {
+              if (swiperInstance && swiperInstance.autoplay) {
+                swiperInstance.autoplay.start();
+              }
+            }, 500);
+          });
         });
-      });
     }
 
     function initializeAutoplayRestart() {
-      document.querySelectorAll('.home-main-slider').forEach((sliderElement) => {
-        let swiperInstance = sliderElement.querySelector('.swiper')?.swiper;
-        if (swiperInstance) {
-          restartAutoplayOnClick(swiperInstance, sliderElement);
-        }
-      });
+      document
+        .querySelectorAll('.home-main-slider')
+        .forEach((sliderElement) => {
+          let swiperInstance = sliderElement.querySelector('.swiper')?.swiper;
+          if (swiperInstance) {
+            restartAutoplayOnClick(swiperInstance, sliderElement);
+          }
+        });
     }
 
     const observerAll = new MutationObserver(() => {
@@ -71,8 +99,6 @@ class Home extends BasePage {
     });
     observerAll.observe(document.body, { childList: true, subtree: true });
     document.addEventListener('DOMContentLoaded', initializeAutoplayRestart);
-
-
 
     // //////
     document.addEventListener('click', function (event) {
@@ -98,88 +124,87 @@ class Home extends BasePage {
     });
   }
   testGInsta() {
-    document.addEventListener('DOMContentLoaded', function () {
-      const openButtons = document.querySelectorAll('.open-video-popup');
-      const popup = document.getElementById('video-popup');
-      const closePopupBtn = document.getElementById('close-popup');
-      const videos = document.querySelectorAll('.story-video');
-      const ownerTexts = document.querySelectorAll('.story-video + span');
-      const closeVideoBtns = document.querySelectorAll('.close-video');
-      openButtons.forEach((btn, index) => {
-        btn.addEventListener('click', (e) => {
-          e.preventDefault();
-          popup.classList.remove('hidden');
-          document.body.style.overflow = 'hidden';
-          setTimeout(() => {
-            const target = videos[index];
-            const ownerText = ownerTexts[index];
-            if (target) {
-              target.scrollIntoView({ behavior: 'smooth', block: 'center' });
-              target.src = target.dataset.src;
-              target.play().catch(() => { });
-              target.muted = false;
-              if (ownerText) {
-                ownerText.classList.remove('hidden');
-              }
+    const openButtons = document.querySelectorAll('.open-video-popup');
+    const popup = document.getElementById('video-popup');
+    const closePopupBtn = document.getElementById('close-popup');
+    const videos = document.querySelectorAll('.story-video');
+    const ownerTexts = document.querySelectorAll('.story-video + span');
+    const closeVideoBtns = document.querySelectorAll('.close-video');
+    openButtons.forEach((btn, index) => {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        popup.classList.remove('hidden');
+        document.body.style.overflow = 'hidden';
+        setTimeout(() => {
+          const target = videos[index];
+          const ownerText = ownerTexts[index];
+          if (target) {
+            target.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            target.src = target.dataset.src;
+            target.play().catch(() => {});
+            target.muted = false;
+            if (ownerText) {
+              ownerText.classList.remove('hidden');
             }
-          }, 100);
-        });
+          }
+        }, 100);
       });
-      closePopupBtn.addEventListener('click', () => {
-        popup.classList.add('hidden');
-        document.body.style.overflow = 'auto';
-        videos.forEach((v) => {
-          v.pause();
-          v.muted = true;
-        });
-        ownerTexts.forEach((text) => text.classList.add('hidden'));
-      });
-      closeVideoBtns.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-          const video = videos[index];
-          video.pause();
-          video.muted = true;
-          video.currentTime = 0;
-          popup.classList.add('hidden');
-          ownerTexts[index].classList.add('hidden');
-          document.body.style.overflow = 'auto';
-        });
-      });
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const video = entry.target;
-            const ownerText = document.getElementById(
-              `owner-${video.dataset.index}`
-            );
-            if (entry.isIntersecting) {
-              videos.forEach((v) => {
-                if (v !== video) {
-                  v.pause();
-                  v.muted = true;
-                }
-              });
-              if (!video.src) video.src = video.dataset.src;
-              video.muted = false;
-              video.play().catch(() => { });
-              if (ownerText) ownerText.classList.remove('hidden');
-            } else {
-              video.pause();
-              video.muted = true;
-              if (ownerText) ownerText.classList.add('hidden');
-            }
-          });
-        },
-        {
-          root: popup,
-          threshold: 0.8
-        }
-      );
-
-      videos.forEach((video) => observer.observe(video));
     });
-  }
 
+    closePopupBtn.addEventListener('click', () => {
+      popup.classList.add('hidden');
+      document.body.style.overflow = 'auto';
+      videos.forEach((v) => {
+        v.pause();
+        v.muted = true;
+      });
+      ownerTexts.forEach((text) => text.classList.add('hidden'));
+    });
+
+    closeVideoBtns.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        const video = videos[index];
+        video.pause();
+        video.muted = true;
+        video.currentTime = 0;
+        popup.classList.add('hidden');
+        ownerTexts[index].classList.add('hidden');
+        document.body.style.overflow = 'auto';
+      });
+    });
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const video = entry.target;
+          const ownerText = document.getElementById(
+            `owner-${video.dataset.index}`
+          );
+          if (entry.isIntersecting) {
+            videos.forEach((v) => {
+              if (v !== video) {
+                v.pause();
+                v.muted = true;
+              }
+            });
+            if (!video.src) video.src = video.dataset.src;
+            video.muted = false;
+            video.play().catch(() => {});
+            if (ownerText) ownerText.classList.remove('hidden');
+          } else {
+            video.pause();
+            video.muted = true;
+            if (ownerText) ownerText.classList.add('hidden');
+          }
+        });
+      },
+      {
+        root: popup,
+        threshold: 0.8
+      }
+    );
+    videos.forEach((video) => observer.observe(video));
+  }
 }
 
 Home.initiateWhenReady(['index']);
